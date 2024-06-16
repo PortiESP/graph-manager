42ad608185d5ab61062b391421b31168d42249b7
PortiESP <portiesp@gmail.com>
2024-06-17 00:40:57 +0200

    edges documented

--- elements/edge.js
+++ elements/edge.js
@@ -1,86 +1,162 @@
+import constants from "../utils/constants"
 import { Element } from "./element"
 
+/**
+ * Edge class
+ * 
+ * Represents a connection between two nodes in the graph
+ * 
+ * This class extends the Element class and defines the common properties and methods for edges
+ * 
+ * @param {Node} src - The source node
+ * @param {Node} dst - The destination node
+ * @param {number} weight - The weight of the edge
+ * @param {boolean} directed - Whether the edge is directed or not
+ * 
+ * **Properties**
+ * 
+ * ---
+ * 
+ * @property {Node} src - The source node
+ * @property {Node} dst - The destination node
+ * @property {number} weight - The weight of the edge (default: 1)
+ * @property {boolean} directed - Whether the edge is directed or not
+ * @property {string} color - The color of the edge
+ * @property {boolean} hover - Whether the edge is being hovered or not
+ * @property {number} thickness - The thickness of the edge
+ * 
+ * **Methods**
+ * 
+ * ---
+ * 
+ * @method draw - Draw the edge
+ * @method distance - Calculate the distance between the edge and a point
+ * @method isHover - Check if the edge is being hovered
+ * @method moveBy - Move the edge by a certain amount
+ * @method clone - Clone the edge
+ * @method equals - Check if two edges are equal
+ * @method delete - Delete the edge
+ */
 export class Edge extends Element{
     constructor(src, dst, weight = 1, directed = false) {
         super()
 
+        // Data properties
         this.src = src
         this.dst = dst
         this.weight = weight
         this.directed = directed
 
-        this.color = "#888"
+        // Style properties
+        this.edgeColor = "#888"
+        this.edgeHoverColor = "#aaa"
+        this.edgeSelectedColor = "aquamarine"
+        this.weightColor = "#eee8"
+        this.weightBackgroundColor = "#8888"
         this.hover = false
         this.thickness = 4
     }
 
+    /**
+     * Draw the edge
+     */
     draw() {
+        // Extract the radius of the nodes (used to draw the edge from border to border of the nodes instead of the center)
         const rSrc = this.src.r
         const rDst = this.dst.r
-        const arrowSize = 15
+        // Arrow head size
+        const arrowSize = constants.ARROW_SIZE
 
         // Calculate the angle between the two nodes
         const angle = Math.atan2(this.dst.y - this.src.y, this.dst.x - this.src.x)
 
         // Calculate the offset from the center of the node
-        const arrowOffset = this.directed ? arrowSize*0.8 : 0
-        const offsetSrc = { x: rSrc * Math.cos(angle), y: rSrc * Math.sin(angle) }
-        const offsetDstArrow = { x: rDst * Math.cos(angle + Math.PI), y: rDst * Math.sin(angle + Math.PI) }
-        const offsetDst = { x: (rDst + arrowOffset) * Math.cos(angle + Math.PI), y: (rDst + arrowOffset) * Math.sin(angle + Math.PI) }  // Flip the angle by adding PI
+        const arrowOffset = this.directed ? arrowSize*0.8 : 0  // If the edge is directed, stop before the border to leave space for the arrow
+        const offsetSrc = { x: rSrc * Math.cos(angle), y: rSrc * Math.sin(angle) } // Calculate the coordinates of the edge from the border of the node
+        const offsetDst = { x: (rDst + arrowOffset) * Math.cos(angle + Math.PI), y: (rDst + arrowOffset) * Math.sin(angle + Math.PI) }  // Calculate the coordinates of the other end of the edge from the border of the node (if the edge is not directed, the arrowOffset is 0)
 
 
         // Draw the edge
         window.ctx.beginPath()
         window.ctx.strokeStyle = 
-            this.selected ? 'aquamarine' : 
-            this.isHover() ? '#aaa' : 
-            this.color
+            this.selected ? this.edgeSelectedColor : // If the edge is selected, draw it in aquamarine
+            this.isHover() ? this.edgeHoverColor :      // If the edge is being hovered, draw it in gray
+            this.edgeColor                     // Otherwise, draw it in its color
         window.ctx.lineWidth = this.thickness
-        window.ctx.moveTo(this.src.x + offsetSrc.x, this.src.y + offsetSrc.y)
-        window.ctx.lineTo(this.dst.x + offsetDst.x, this.dst.y + offsetDst.y)
+        window.ctx.moveTo(this.src.x + offsetSrc.x, this.src.y + offsetSrc.y)  // Move to the source node
+        window.ctx.lineTo(this.dst.x + offsetDst.x, this.dst.y + offsetDst.y)  // Draw a line to the destination node
         window.ctx.stroke()
 
         // If the edge is directed, draw an arrow
         if (this.directed) {
             const arrowAngle = Math.PI / 6
+            const offsetDstArrow = { x: rDst * Math.cos(angle + Math.PI), y: rDst * Math.sin(angle + Math.PI) } // Calculate the coordinates of the arrow from the border of the node
 
+            // Draw the arrow
             window.ctx.beginPath()
-            window.ctx.fillStyle = this.isHover() ? "red" : this.color
+            window.ctx.fillStyle = this.isHover() ? this.edgeHoverColor : this.edgeColor
             window.ctx.moveTo(this.dst.x + offsetDstArrow.x, this.dst.y + offsetDstArrow.y)
             window.ctx.lineTo(this.dst.x + offsetDstArrow.x - arrowSize * Math.cos(angle - arrowAngle), this.dst.y + offsetDstArrow.y - arrowSize * Math.sin(angle - arrowAngle))
             window.ctx.lineTo(this.dst.x + offsetDstArrow.x - arrowSize * Math.cos(angle + arrowAngle), this.dst.y + offsetDstArrow.y - arrowSize * Math.sin(angle + arrowAngle))
             window.ctx.fill()
         }
 
-        // Draw the weight
+        // Paint the weight of the edge in the middle of the edge
         if (window.graph.showWeights) {
+            // Calculate the center of the edge
             const centerX = (this.src.x + this.dst.x) / 2
             const centerY = (this.src.y + this.dst.y) / 2
             
-            window.ctx.font = "16px Arial"   
+            const fontSize = constants.EDGE_WEIGHT_FONT_SIZE
+            const boxSize = constants.EDGE_WEIGHT_BOX_SIZE + String(this.weight).length * 4
+            window.ctx.font = fontSize + "px Arial"   
             window.ctx.textAlign = "center"
             window.ctx.textBaseline = "middle"
-            window.ctx.fillStyle = "#bbb8"
-            window.ctx.fillRect(centerX - 15, centerY - 16*0.75, 30, 20)
-            window.ctx.fillStyle = "#000"
+            window.ctx.fillStyle = this.weightBackgroundColor
+            window.ctx.fillRect(centerX - boxSize, centerY - fontSize*0.75, boxSize*2, fontSize*1.25)  // The values hardcoded are calculated to center the box
+            window.ctx.fillStyle = this.weightColor
             window.ctx.fillText(this.weight, centerX, centerY)
         }
             
     }
 
+    /**
+     * Calculate the distance between the closest point of the edge and a point
+     * 
+     * @param {Number} x Coordinate x of the point
+     * @param {Number} y Coordinate y of the point
+     * @returns The distance between the edge and the point
+     */
     distance(x, y) {
-        // Calculate the distance from the edge to the point
+
+        // Check if point is inside the bounding box of the edge
+        const x1 = Math.min(this.src.x, this.dst.x)
+        const x2 = Math.max(this.src.x, this.dst.x)
+        const y1 = Math.min(this.src.y, this.dst.y)
+        const y2 = Math.max(this.src.y, this.dst.y)
+
+        // If the point is outside the bounding box, return the distance to the closest end of the edge
+        if (x < x1 || x > x2 || y < y1 || y > y2) {
+            // Return the distance to the closest node
+            return Math.min(
+                Math.sqrt((this.src.x - x) ** 2 + (this.src.y - y) ** 2), // Distance to the source node
+                Math.sqrt((this.dst.x - x) ** 2 + (this.dst.y - y) ** 2)  // Distance to the destination node
+            )
+        }
+
+        // If the point if inside the bounding box, calculate the distance to the line
+        // Distance from a point to a line formula: |Ax + By + C| / sqrt(A^2 + B^2)
         const A = this.src.y - this.dst.y // y1 - y2
         const B = this.dst.x - this.src.x // x2 - x1
         const C = this.src.x * this.dst.y - this.dst.x * this.src.y // x1*y2 - x2*y1
-
-        const dist = Math.abs(A * x + B * y + C) / Math.sqrt(A * A + B * B)
-
-        // Check if the point is inside the edge
-
-        return dist
+        return Math.abs(A * x + B * y + C) / Math.sqrt(A * A + B * B)
     }
 
+    /**
+     * Similar to the distance method, but returns a boolean indicating whether the edge is being hovered or not. 
+     * Also, it only checks if the point is inside the bounding box of the edge
+     * @returns Boolean. Whether the edge is being hovered or not
+     */
     isHover() {
         const x = window.cvs.x
         const y = window.cvs.y
@@ -99,32 +175,50 @@ export class Edge extends Element{
         return this.distance(window.cvs.x, window.cvs.y) <= this.thickness  
     }
 
-    // This method does nothing for edges
+    /**
+     * This method is not implemented for edges, but its necessary to keep the interface consistent with the Node class. 
+     * 
+     * [i] This is due to the ability to select multiple elements at once being some of those elements nodes and edges, but edges position is determined by the position of the nodes they are connected to.
+     */
     moveBy(){}
 
-    // Clone the edge
+    /**
+     * Creates a clone of the edge
+     * @returns Edge. A clone of the edge
+     */
     clone() {
         const aux = new Edge(this.src, this.dst, this.weight, this.directed)
         aux.color = this.color
         aux.thickness = this.thickness
+        aux.edgeColor = this.edgeColor
+        aux.edgeHoverColor = this.edgeHoverColor
+        aux.edgeSelectedColor = this.edgeSelectedColor
+        aux.weightColor = this.weightColor
+        aux.weightBackgroundColor = this.weightBackgroundColor
+        aux.hover = this.hover
+        aux.thickness = this.thickness
+
 
         return aux
     }
 
-    // Check if two edges are equal
+    /**
+     * Check if two edges are equal. Ignores the style properties and hover property
+     * @param {Edge} edge - The edge to compare with
+     * @returns Boolean. Whether the two edges are equal or not
+     */
     equals(edge) {
         return [ 
             this.src === edge.src,
             this.dst === edge.dst,
             this.weight === edge.weight,
             this.directed === edge.directed,
-            this.color === edge.color,
-            this.thickness === edge.thickness,
-            this.selected === edge.selected,
         ].every(e => e)
     }
 
-    // Delete the edge
+    /**
+     * Delete the edge from the graph by removing it from the edges array
+     */
     delete() {
         window.graph.edges = window.graph.edges.filter(e => e !== this)
     }
--- utils/constants.js
+++ utils/constants.js
@@ -7,4 +7,8 @@ export default {
     EDGE_DETECTION_RADIUS: 10,
     DOUBLE_CLICK_DELAY: 400,
     ZOOM_LEVELS: [0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.1, 1.2, 1.3, 1.4, 1.5],
+    CLEAN_MARGIN: 100,
+    ARROW_SIZE: 15,
+    EDGE_WEIGHT_BOX_SIZE: 10,  // Horizontal padding of the box containing the weight of the edge
+    EDGE_WEIGHT_FONT_SIZE: 16, // Font size of the weight of the edge
 }
