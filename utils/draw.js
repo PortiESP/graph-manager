8fba73ae960b28dde6682807c5e8ba2bc1af6d99
PortiESP <portiesp@gmail.com>
2024-06-16 11:06:10 +0200

    grid

--- utils/draw.js
+++ utils/draw.js
@@ -1,4 +1,6 @@
 export default function drawAll(){
+    drawGrid()
+
     // --- Edges ---
     // Draw edges
     window.graph.edges.forEach(e => {
@@ -25,5 +27,43 @@ export default function drawAll(){
         window.ctx.beginPath()
         window.ctx.arc(window.cvs.x, window.cvs.y, 30, 0, Math.PI * 2)
         window.ctx.fill()
+    }
+}
+
+
+export function drawGrid(){
+    const ctx = window.ctx
+    const cvs = window.cvs
+
+    const gridSize = 50
+    const gridColor = "#ddd"
+    const gridThickness = 1
+
+    // Draw the grid
+    ctx.strokeStyle = gridColor
+    ctx.lineWidth = gridThickness
+    const width = cvs.$canvas.width
+    const height = cvs.$canvas.height
+    const offsetX = cvs.canvasDragOffset.x
+    const offsetY = cvs.canvasDragOffset.y
+    const iniX = -(2*gridSize) - Math.floor(offsetX / gridSize) * gridSize
+    const iniY = -(2*gridSize) - Math.floor(offsetY / gridSize) * gridSize
+    const finX = (2*gridSize) + width - Math.floor(offsetX / gridSize) * gridSize
+    const finY = (2*gridSize) + height - Math.floor(offsetY / gridSize) * gridSize
+    for (let x = iniX; x < finX; x += gridSize) {
+        ctx.beginPath()
+        ctx.moveTo(x, iniY)
+        ctx.lineTo(x, finY)
+        ctx.stroke()
+        ctx.fillStyle = gridColor
+        ctx.fillText(x, x+10, -offsetY+10)
+    }
+    for (let y = iniY; y < finY; y += gridSize) {
+        ctx.beginPath()
+        ctx.moveTo(iniX, y)
+        ctx.lineTo(finX, y)
+        ctx.stroke()
+        ctx.fillStyle = gridColor
+        ctx.fillText(y, -offsetX+4, y+20)
     }
 }
