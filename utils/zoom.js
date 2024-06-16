74cd29f735fccaee0d31903da036bb55c0a0989d
PortiESP <portiesp@gmail.com>
2024-06-16 18:13:36 +0200

    zoom almos ready

--- utils/zoom.js
+++ utils/zoom.js
@@ -23,7 +23,20 @@ export function getViewBox(){
 export function zoomCenter(zoomIn){
     const zoomFactor = zoomIn ? 1.1 : 0.9
     const newZoom = zoomIn ? window.graph.zoom * zoomFactor : window.graph.zoom * zoomFactor
+    const {x, y} = getViewBox()
+    const centerX = x + window.cvs.$canvas.width/2
+    const centerY = y + window.cvs.$canvas.height/2
+    const dx =  centerX
+    const dy =  centerY
+
+    window.ctx.translate(x, y)
     window.ctx.scale(zoomFactor, zoomFactor)
+    window.ctx.translate(-x, -y)
+    window.ctx.translate(dx,dy)
+
+    window.graph.canvasDragOffset.x += dx
+    window.graph.canvasDragOffset.y += dy
+    
     window.graph.zoom = newZoom
 }
 
