1e8e4897d6a5b5facee150f1a34d3b1a0a9c654f
PortiESP <portiesp@gmail.com>
2024-06-17 02:35:55 +0200

    globals doc

--- globals.js
+++ globals.js
@@ -2,6 +2,29 @@ import constants from "./utils/constants"
 import CONSTANTS from "./utils/constants"
 import { activateTool } from "./utils/tools/tools_callbacks"
 
+/**
+ * Sets up the global variables used by the graph. These variables are stored in the `window.graph` object.
+ * 
+ * This function should be called in the `setupCanvas` function in the `Canvas` component.
+ * 
+ * **Properties** 
+ * 
+ * ---
+ * 
+ * @property {Array} nodes - All nodes in the graph
+ * @property {Array} selected - Selected nodes
+ * @property {boolean} prevent_deselect - Prevent deselecting nodes (used after dragging nodes)
+ * @property {boolean} showWeights - Show weights on edges
+ * @property {boolean} newNode - New node being created
+ * @property {Object} dragging_edge - Dragging an edge
+ * @property {Array} edges - All edges
+ * @property {Object} newEdgeScr - Source node for when the used is creating a new edge
+ * @property {Array} memento - Memento stack
+ * @property {Array} mementoRedo - Redo stack
+ * @property {Object} canvasDragOffset - Canvas drag offset
+ * @property {number} zoom - Zoom factor
+ * @property {number} zoomLevel - Zoom level (index of the zoom factor in the zoom levels array at the constants file)
+ */
 export function setupGraphGlobals() {
     window.graph = {}
 
@@ -32,8 +55,7 @@ export function setupGraphGlobals() {
     window.graph.zoom = 1 // Zoom factor
     window.graph.zoomLevel = constants.ZOOM_LEVELS.indexOf(1) // Zoom level (index of the zoom factor in the zoom levels array at the constants file)
 
-
-    // Tools
+    // Activate the default tool
     activateTool(CONSTANTS.DEFAULT_TOOL) // Activate the default tool
 
 }
