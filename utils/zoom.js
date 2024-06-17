c7bedce4c0f0dc9de7be2122279ef81bbabbe471
PortiESP <portiesp@gmail.com>
2024-06-17 02:32:20 +0200

    zoom documented

--- utils/zoom.js
+++ utils/zoom.js
@@ -1,11 +1,24 @@
 import constants from "./constants"
 
+/**
+ * Resets the zoom of the graph to the default value (1).
+ */
 export function resetZoom(){
     window.ctx.scale(1/window.graph.zoom, 1/window.graph.zoom)
-    // window.graph.canvasDragOffset = {x: 0, y: 0}
     window.graph.zoom = 1
 }
 
+/**
+ * Calculates some values related to the coordinates and dimensions of the canvas.
+ * 
+ * @returns {Object} Returns an object with the following properties:
+ * - x: The x coordinate of the top-left corner of the canvas
+ * - y: The y coordinate of the top-left corner of the canvas
+ * - width: The width of the canvas
+ * - height: The height of the canvas
+ * - x2: The x coordinate of the bottom-right corner of the canvas
+ * - y2: The y coordinate of the bottom-right corner of the canvas
+ */
 export function getViewBox(){
     const {x, y} = window.graph.canvasDragOffset
     const {width, height} = window.cvs.$canvas
@@ -19,37 +32,54 @@ export function getViewBox(){
     }
 }
 
+/**
+ * Zooms the graph to a specific level.
+ * 
+ * @param {Boolean} zoomIn - Whether to zoom in or out. If true, zooms in, otherwise zooms out.
+ */
 export function zoomCenter(zoomIn){
+    // Determine the zoom factor
     const zoomFactor = zoomIn ? 1.1 : 0.9
+    // Calculate the new zoom level
     const newZoom = zoomIn ? window.graph.zoom * zoomFactor : window.graph.zoom * zoomFactor
+
+    // Calculate the new canvas position
     const {x, y, width, height} = getViewBox()
-    const centerX = x + window.cvs.$canvas.width/2
-    const centerY = y + window.cvs.$canvas.height/2
-    const userX = window.cvs.x
+    const userX = window.cvs.x  // Current mouse position
     const userY = window.cvs.y
-    const dx = -(width*zoomFactor - width)/2
+    const dx = -(width*zoomFactor - width)/2  // Calculate the offset to center the zoom (padding)
     const dy = -(width*zoomFactor - width)/2
 
-    window.ctx.translate(x, y)
-    window.ctx.scale(zoomFactor, zoomFactor)
-    window.ctx.translate(-x, -y)
-    window.ctx.translate(dx,dy)
+    // Apply the zoom (we need to revert the canvas position since the zoom is applied from the top-left corner of the canvas)
+    window.ctx.translate(x, y) // Reset the canvas position
+    window.ctx.scale(zoomFactor, zoomFactor) // Apply the zoom
+    window.ctx.translate(-x, -y) // Apply again the canvas position
+    window.ctx.translate(dx,dy)  // Apply the padding
 
+    // Update the canvas drag offset
     window.graph.canvasDragOffset.x += dx
     window.graph.canvasDragOffset.y += dy
     
+    // Update the zoom level
     window.graph.zoom = newZoom
 
-    // Update mouse position
+    // Update mouse position (to keep mouse position unchanged after zooming, this is necessary due to the translations applied to the canvas, this translations move the canvas, but the mouse position is not updated until the next mouseMove event so we need to update it manually just in case the user does not move the mouse after zooming)
     window.cvs.x = userX-dx
     window.cvs.y = userY-dy
 }
 
+/**
+ * Zooms in the graph.
+ */
 export function zoomIn(){
     const {x, y} = window.cvs
     zoomCenter(true)
 }
 
+
+/**
+ * Zooms out the graph.
+ */
 export function zoomOut(){
     const {x, y} = window.cvs
     zoomCenter(false)
