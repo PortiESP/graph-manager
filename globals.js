a17fccc9cbdf0b99a432aeca435b1f8fbe7f36a9
PortiESP <portiesp@gmail.com>
2024-06-22 19:29:13 +0200

    globals and view docs

--- globals.js
+++ globals.js
@@ -12,13 +12,18 @@ import { activateTool } from "./utils/tools/tools_callbacks"
  * 
  * @property {Array} nodes - All nodes in the graph
  * @property {Array} selected - Selected nodes
+ * @property {Element} selectionBox - Object representing the selection box: {x1, y1, x2, y2}
  * @property {boolean} prevent_deselect - Prevent deselecting nodes (used after dragging nodes)
  * @property {boolean} showWeights - Show weights on edges
+ * @property {boolean} snapToGrid - Snap elements to the grid
+ * @property {Object} snapReference - Reference point for snapping (used when dragging nodes while on snap mode)
  * @property {boolean} newNode - New node being created
  * @property {Array} edges - All edges
  * @property {Object} newEdgeScr - Source node for when the used is creating a new edge
  * @property {Array} memento - Memento stack
  * @property {Array} mementoRedo - Redo stack
+ * @property {Element} doubleClickTarget - The target of the double click event (set to the target of the mouse down event when a double click is detected, and reset to null on the next mouse up event)
+ * @property {Array} info - Information elements
  */
 export function setupGraphGlobals() {
     window.graph = {}
@@ -26,7 +31,7 @@ export function setupGraphGlobals() {
     // Graph data
     window.graph.nodes = [] // All nodes
     window.graph.selected = [] // Selected nodes
-    window.graph.selectionBox = null // Selection box
+    window.graph.selectionBox = null // Object representing the selection box: {x1, y1, x2, y2}
 
     // Config
     window.graph.prevent_deselect = false // Prevent deselecting nodes (used after dragging nodes)
--- utils/view.js
+++ utils/view.js
@@ -1,26 +1,38 @@
 import { panTo } from "../../canvas/utils/pan"
 import { getViewBox, zoomToFit } from "../../canvas/utils/zoom"
 
+
+/**
+ * Pans the canvas to focus on the given element by centering it on the screen.
+ * 
+ * @param {Element} element - The element to focus on
+ */
 export function focusOnElement(element) {
-    // // Get the element's bounding box
+    // Get the element's position
     const {x: eX, y: eY} = element
 
-    // // Get the current viewbox
+    // Get the canvas dimensions
     const {width, height} = getViewBox()
 
-    // // Calculate the new viewbox
+    // Calculate the new position of the canvas
     const newX = eX - width / 2
     const newY = eY - height / 2
 
+    // Pan to the new position
     panTo(newX, newY)
 }
 
+
+/**
+ * Pans the canvas to focus on all the elements by centering them on the screen.
+ */
 export function focusOnAll(){
     let x1 = Infinity
     let y1 = Infinity
     let x2 = -Infinity
     let y2 = -Infinity
 
+    // Iterate over all the nodes to get the coordinates of a bounding box that contains all the nodes
     for (const node of window.graph.nodes){
         x1 = Math.min(x1, node.x-node.r*2)
         y1 = Math.min(y1, node.y-node.r*2)
@@ -28,16 +40,18 @@ export function focusOnAll(){
         y2 = Math.max(y2, node.y+node.r*2)
     }
 
+    // Dimensions of the bounding box
     const width = x2 - x1
     const height = y2 - y1
     
     // Adjust the zoom level to fit all the nodes
     zoomToFit(width, height)
 
-    // Calculate the new viewbox
+    // Calculate the new position of the canvas
     const {width: canvasWidth, height: canvasHeight} = getViewBox()
     const newX = x1 - (canvasWidth - width) / 2
     const newY = y1 - (canvasHeight - height) / 2
 
+    // Pan to the new position
     panTo(newX, newY)
 }
