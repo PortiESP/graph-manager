c56611b12c0ab4b8f4e9725337109995a7480cc9
PortiESP <portiesp@gmail.com>
2024-06-30 18:31:39 +0200

    refeactor generate graph obj

--- utils/algorithms/algorithm_utils/generate_graph.js
+++ utils/algorithms/algorithm_utils/generate_graph.js
@@ -4,28 +4,23 @@
  * @returns {Object} An object with the nodes as keys and an array of edges as values: {Node: [Edge, ...], ...}
  */
 export function generateGraphArray(){
-    const graph = {}
+    const graph = {
+        nodes: [],
+        edges: []
+    }
 
     // Initialize the graph as an empty array for each node
     window.graph.nodes.forEach(node => {
-        graph[node] = []
+        graph.nodes.push({x: node.x, y: node.y, label: node.label, r: node.r})
     })
 
     // Fill the graph with the edges
     window.graph.edges.forEach(edge => {
         // Add the edge to the graph
-        graph[edge.src].push(edge)
-
-        // Add the edge also to the destination node if the edge is not directed
-        if (!edge.directed) {
-            // Clone the edge and swap the src and dst
-            const newE = edge.clone()
-            newE.src = edge.dst
-            newE.dst = edge.src
-
-            // Add the new edge to the graph
-            graph[edge.dst].push(newE)
-        }
+        const objEdge = {src: edge.src, dst: edge.dst}
+        if(edge.weight !== undefined) objEdge.weight = edge.weight
+        if(edge.directed) objEdge.directed = edge.directed
+        graph.edges.push(objEdge)
     })
 
     return graph
@@ -50,4 +45,13 @@ export function generateEdgeAndNodesList(){
     const eList = generateEdgeList()
     const nList = window.graph.nodes.map(n => n.toString()).filter(n => !eList.join("").includes(n))
     return eList.concat(nList)
+}
+
+
+export function generateURL(){
+    const url = new URL(window.location.href)
+    const graph = generateEdgeAndNodesList().join("_")
+    url.searchParams.set("graph", graph)
+
+    return url.href
 }
