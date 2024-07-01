b7275610db5cb17b557f6e38969d1cfb60ec17e8
PortiESP <portiesp@gmail.com>
2024-07-01 12:52:01 +0200

    updated svg generator, generates styles

--- utils/algorithms/algorithm_utils/generate_graph.js
+++ utils/algorithms/algorithm_utils/generate_graph.js
@@ -1,3 +1,4 @@
+import constants from "../../constants"
 import { getBoundingBoxOfAllNodes } from "../../view"
 
 /**
@@ -65,7 +66,6 @@ export function generateURL(){
 export function generateSVG(elementId){
     const svg = document.getElementById(elementId)
     const svgNS = "http://www.w3.org/2000/svg"
-    const xmlns = "http://www.w3.org/2000/xmlns/"
     const xlinkns = "http://www.w3.org/1999/xlink"
 
     // Clear the SVG
@@ -76,7 +76,8 @@ export function generateSVG(elementId){
     svg.setAttribute("xmlns:xlink", xlinkns)
     svg.setAttribute("version", "1.1")
     const {x1, y1, width, height} = getBoundingBoxOfAllNodes()
-    svg.setAttribute("viewBox", `${x1} ${y1} ${width} ${height}`)
+    const margin = 1
+    svg.setAttribute("viewBox", `${x1-margin} ${y1-margin} ${width+margin*2} ${height+margin*2}`)
 
     // Create the defs element
     const defs = document.createElementNS(svgNS, "defs")
@@ -84,40 +85,15 @@ export function generateSVG(elementId){
 
     // Create the style element
     const style = document.createElementNS(svgNS, "style")
-    style.textContent = `
-        .node {
-            fill: #fff;
-            stroke: #000;
-            stroke-width: 2px;
-        }
-        .edge {
-            stroke: #000;
-            stroke-width: 2px;
-        }
-        .label {
-            font-family: Arial, sans-serif;
-            font-size: 12px;
-        }
-    `
+
+    style.textContent = generateCSS()
     defs.appendChild(style)
 
-    // Create the nodes
-    window.graph.nodes.forEach(node => {
-        const circle = document.createElementNS(svgNS, "circle")
-        circle.setAttribute("class", "node")
-        circle.setAttribute("cx", node.x)
-        circle.setAttribute("cy", node.y)
-        circle.setAttribute("r", node.r)
-        circle.setAttribute("fill", node.color)
-        svg.appendChild(circle)
+    const nodes = []
+    const nodesLabels = []
+    const edges = []
+    const edgesLabels = []
 
-        const text = document.createElementNS(svgNS, "text")
-        text.setAttribute("class", "label")
-        text.setAttribute("x", node.x)
-        text.setAttribute("y", node.y)
-        text.textContent = node.label
-        svg.appendChild(text)
-    })
 
     // Create the edges
     window.graph.edges.forEach(edge => {
@@ -128,13 +104,106 @@ export function generateSVG(elementId){
         line.setAttribute("x2", edge.dst.x)
         line.setAttribute("y2", edge.dst.y)
         line.setAttribute("stroke", edge.color)
-        svg.appendChild(line)
+        line.setAttribute("id", edge.id)
+        edges.push(line)
+
+        const textBox = document.createElementNS(svgNS, "rect")
+        textBox.setAttribute("class", "label")
+        const centerX = (edge.src.x + edge.dst.x) / 2
+        const centerY = (edge.src.y + edge.dst.y) / 2
+        const fontSize = edge.weightFontSize
+        const boxSize = constants.EDGE_WEIGHT_BOX_SIZE + String(edge.weight).length * 4
+        textBox.setAttribute("x", centerX - boxSize)
+        textBox.setAttribute("y", centerY - fontSize*0.75)
+        textBox.setAttribute("width", boxSize*2)
+        textBox.setAttribute("height", fontSize*1.25)
+        textBox.setAttribute("id", "box-"+edge.id)
+        edgesLabels.push(textBox)
 
         const text = document.createElementNS(svgNS, "text")
         text.setAttribute("class", "label")
         text.setAttribute("x", (edge.src.x + edge.dst.x) / 2)
         text.setAttribute("y", (edge.src.y + edge.dst.y) / 2)
+        text.setAttribute("id", "label-"+edge.id)
         text.textContent = edge.weight
-        svg.appendChild(text)
+        edgesLabels.push(text)
     })
+
+    // Create the nodes
+    window.graph.nodes.forEach(node => {
+        const circle = document.createElementNS(svgNS, "circle")
+        circle.setAttribute("class", "node")
+        circle.setAttribute("cx", node.x)
+        circle.setAttribute("cy", node.y)
+        circle.setAttribute("r", node.r)
+        circle.setAttribute("id", node.id)
+        nodes.push(circle)
+
+        const text = document.createElementNS(svgNS, "text")
+        text.setAttribute("class", "node_label")
+        text.setAttribute("x", node.x)
+        text.setAttribute("y", node.y)
+        text.setAttribute("id", "label-"+node.id)
+        text.textContent = node.label
+        nodesLabels.push(text)
+    })
+
+    // Append the nodes and edges to the SVG (in this order to make sure the edges are behind the nodes)
+    edges.forEach(edge => svg.appendChild(edge))
+    nodes.forEach(node => svg.appendChild(node))
+    nodesLabels.forEach(label => svg.appendChild(label))
+    edgesLabels.forEach(label => svg.appendChild(label))
+    
+}
+
+
+export function generateCSS(){
+
+    const styles = []
+
+    window.graph.edges.forEach(edge => {
+        const edge_styles = {
+            stroke: edge.color,
+            "stroke-width": edge.thickness,
+        }
+
+        const edge_label_styles = {
+            fill: edge.weightColor,
+            "font-size": edge.weightFontSize,
+            "text-anchor": "middle",
+            "dominant-baseline": "middle",
+        }
+
+        const edge_box_styles = {
+            fill: edge.weightBackgroundColor,
+            // Bring to the front
+            "z-index": 10,
+        }
+
+        styles.push(`.edge#${edge.id} {${Object.entries(edge_styles).map(([key, value]) => `${key}: ${value}`).join("; ")}}`)
+        styles.push(`.label#label-${edge.id} {${Object.entries(edge_label_styles).map(([key, value]) => `${key}: ${value}`).join("; ")}}`)
+        styles.push(`.label#box-${edge.id} {${Object.entries(edge_box_styles).map(([key, value]) => `${key}: ${value}`).join("; ")}}`)
+
+    })
+
+    window.graph.nodes.forEach(node => {
+        const node_styles = {
+            fill: node.backgroundColor,
+            stroke: node.borderColor,
+            "stroke-width": node.borderWidth,
+            "stroke-color": node.borderColor,
+        }
+
+        const node_label_styles = {
+            fill: node.labelColor,
+            "font-size": node.fontSize,
+            "text-anchor": "middle",
+            "dominant-baseline": "middle",
+        }
+
+        styles.push(`.node#${node.id} {${Object.entries(node_styles).map(([key, value]) => `${key}: ${value}`).join("; ")}}`)
+        styles.push(`.node_label#label-${node.id} {${Object.entries(node_label_styles).map(([key, value]) => `${key}: ${value}`).join("; ")}}`)
+    })
+
+    return styles.join("\n")
 }
